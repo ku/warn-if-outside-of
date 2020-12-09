8a5bb15c37d10ac3d17a51494f9b8d249b93ecd9
ku KUMAGAI <ku0522a@gmail.com>
2020-12-09 15:28:18 +0900

    dump

--- src/hello.js
+++ src/hello.js
@@ -1,12 +1,29 @@
 
-const core = require('@actions/core');
-const github = require('@actions/github');
+const core = require('@actions/core')
+const github = require('@actions/github')
+const Octokit = require('@octokit/rest')
+
+class CommentIfOutsideOf {
+
+  constructor(payload) {
+
+  }
+
+}
 
 try {
   const payload = JSON.stringify(github.context.payload, null, '  ')
   console.log(`The event payload: ${payload}`);
 
+  const env = JSON.stringify(process.env, null, '  ')
+  console.log(env)
+
+  const octokit = new Octokit({
+    auth: `token ${process.env.GITHUB_TOKEN}`
+  })
+
   core.setOutput("assignee", 'x');
 } catch (error) {
   core.setFailed(error.message);
 }
+
