ebdb569ca7b14bd504c0ed4ec854f0e75bd37f07
ku KUMAGAI <ku0522a@gmail.com>
2020-12-09 14:52:44 +0900

    k

--- src/hello.js
+++ src/hello.js
@@ -3,8 +3,9 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 try {
-  const payload = JSON.stringify(github.context.payload, null, 2)
+  const payload = JSON.stringify(github.context.payload, null, '  ')
   console.log(`The event payload: ${payload}`);
+
   core.setOutput("assignee", 'x');
 } catch (error) {
   core.setFailed(error.message);
