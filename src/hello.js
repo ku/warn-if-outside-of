f860d492f889a832cd635c20d93e4217c22d1ca7
ku KUMAGAI <ku0522a@gmail.com>
2020-12-09 16:49:11 +0900

    input

--- src/hello.js
+++ src/hello.js
@@ -19,17 +19,47 @@ class CommentIfOutsideOf {
       repo: this.context.repository.name,
       pull_number: this.context.pull_request.number,
     });
-
-    console.log(JSON.stringify(r, null, '  '))
+    this.context = JSON.parse(payload)
+    this.octokit = new Octokit({
+      auth: `token ${process.env.GITHUB_TOKEN}`
+    })
   }
 
+  async listFiles() {
+    return this.octokit.pulls.listFiles({
+      owner: this.context.repository.owner.login,
+      repo: this.context.repository.name,
+      pull_number: this.context.pull_request.number,
+    });
+  }
+
+  async main() {
+    const response = await this.listFiles()
+    const filenames = response.data.map( (f) => f.filename )
+    console.log(JSON.stringify(filenames, null, '  '))
+  }
 }
 
 try {
+  console.log(JSON.stringify( github.event.inputs, null, '  ' ))
+
   const env = JSON.stringify(process.env.GITHUB_TOKEN, null, '  ')
   console.log(`token ${process.env.GITHUB_TOKEN}`)
 
-  const app = new CommentIfOutsideOf(github.context.payload)
+  const octokit = new Octokit({
+    auth: `token ${process.env.GITHUB_TOKEN}`
+  })
+
+  let payload
+  if(process.env.USER === 'ku') {
+    const fs = require('fs');
+    payload = fs.readFileSync(0, 'utf-8');
+  } else {
+    payload = github.context.payload
+  }
+
+
+  const app = new CommentIfOutsideOf(payload)
   app.main()
 
   core.setOutput("assignee", 'x');
